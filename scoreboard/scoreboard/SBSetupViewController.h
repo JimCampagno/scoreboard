9d921e8f468439d6b283dae872b1cc9412b03170
JimCampagno <jimcampagno@gmail.com>
2015-05-24 20:01:14 -0400

    Add Digit array

--- scoreboard/scoreboard/SBSetupViewController.h
+++ scoreboard/scoreboard/SBSetupViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SBSetupViewController : UIViewController
+@interface SBSetupViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- scoreboard/scoreboard/SBSetupViewController.m
+++ scoreboard/scoreboard/SBSetupViewController.m
@@ -12,34 +12,92 @@
 @interface SBSetupViewController ()
 
 
+@property (weak, nonatomic) IBOutlet UIView *displayJoinGameDigits;
 @property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *joinGameNumbers;
 @property (weak, nonatomic) IBOutlet UITextField *enterName;
+- (IBAction)connect:(id)sender;
+- (IBAction)createGame:(id)sender;
+- (IBAction)joinGame:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *invisibleDigits;
+@property (strong, nonatomic) NSMutableArray *holdingTheDigits;
+@property (weak, nonatomic) IBOutlet UIButton *connectProp;
 
 @end
 
 @implementation SBSetupViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     
+    self.invisibleDigits.delegate = self;
+    
     [SBUILabelHelper setupBorderOfLabelsWithArrayOfLabels:self.joinGameNumbers];
-
-  
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if ([string isEqualToString:@""]) {
+        
+        if ([self.holdingTheDigits count] >= 1) {
+            UILabel *label = self.joinGameNumbers[[self.holdingTheDigits count] - 1];
+            label.text = @"";
+            [self.holdingTheDigits removeLastObject];
+            self.connectProp.enabled = NO;
+        }
+        
+    } else if ([self.holdingTheDigits count] < 6){
+        
+        [self.holdingTheDigits addObject:string];
+        UILabel *label = self.joinGameNumbers[[self.holdingTheDigits count] - 1];
+        label.text = string;
+        
+        if ([self.holdingTheDigits count] == 6) {
+            self.connectProp.enabled = YES;
+        }
+    }
+    
+    return YES;
 }
-*/
+
+- (IBAction)connect:(id)sender {
+    
+    NSLog(@"The connect button was pressed!");
+}
+
+- (IBAction)createGame:(id)sender {
+    
+    NSLog(@"The createGame button was pressed.");
+}
+
+- (IBAction)joinGame:(id)sender {
+    
+    [self.invisibleDigits becomeFirstResponder];
+    
+    NSLog(@"The joinGame button was pressed.");
+}
+
+- (NSMutableArray *)holdingTheDigits {
+    
+    if (!_holdingTheDigits) {
+        _holdingTheDigits = [[NSMutableArray alloc] init];
+    }
+    return _holdingTheDigits;
+}
 
 
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 @end
