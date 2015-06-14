c74b140ec7c3fbfda89afda425426cd31431fd13
JimCampagno <jimcampagno@gmail.com>
2015-06-14 17:34:05 -0400

    nib change

---
+++ scoreboard/scoreboard/SBPlayerScorecard.h
@@ -0,0 +1,13 @@
+//
+//  SBPlayerScorecard.h
+//  scoreboard
+//
+//  Created by Jim Campagno on 6/14/15.
+//  Copyright (c) 2015 Gamesmith, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBPlayerScorecard : UIView
+
+@end
