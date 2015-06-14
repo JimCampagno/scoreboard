abe416beef6a8b2eb0216927c0e50d4633627016
JimCampagno <jimcampagno@gmail.com>
2015-06-14 12:33:58 -0400

    picker view updates

--- scoreboard/scoreboard/SBGameScreenViewController.h
+++ scoreboard/scoreboard/SBGameScreenViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SBGameScreenViewController : UIViewController
+@interface SBGameScreenViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
 
 @end
--- scoreboard/scoreboard/SBGameScreenViewController.m
+++ scoreboard/scoreboard/SBGameScreenViewController.m
@@ -13,6 +13,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *mainMonsterName;
 @property (weak, nonatomic) IBOutlet UIImageView *mainMonsterImage;
 @property (weak, nonatomic) IBOutlet UIView *mainMonsterView;
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
+
+@property (strong, nonatomic) NSMutableArray *pickerData;
 
 @end
 
@@ -20,6 +23,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.picker.dataSource = self;
+    self.picker.delegate = self;
+    
+    
     
 
 //    CGFloat width = CGRectGetWidth(self.mainMonsterView.bounds);
@@ -34,11 +42,39 @@
     
 }
 
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    
+    
+    return [self.pickerData count];
+}
+
+
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+    return self.pickerData[row];
+}
+
+
+- (NSMutableArray *)pickerData {
+    if (!_pickerData) {
+        
+        _pickerData = [[NSMutableArray alloc] init];
+        _pickerData = [@[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8"] mutableCopy];
+    }
+    
+    return _pickerData;
+}
+
 -(void)viewDidAppear:(BOOL)animated {
     
     CGFloat width = CGRectGetWidth(self.mainMonsterView.bounds);
     CGFloat height = CGRectGetHeight(self.mainMonsterView.bounds);
-    NSLog(@"The width is : %f and the height is %f", width, height);
+//    NSLog(@"The width is : %f and the height is %f", width, height);
     UIImageView *dot =[[UIImageView alloc] initWithFrame:CGRectMake(0,height/6,height/1.5,height/1.5)];
     dot.backgroundColor = [UIColor blackColor];
     [self.mainMonsterView addSubview:dot];
