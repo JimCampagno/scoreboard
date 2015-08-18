a6027b3fec3f663c5a6f8998cf1671d4bffcc3fb
JimCampagno <jimcampagno@gmail.com>
2015-08-18 15:48:04 -0400

    change screen controller

---
+++ scoreboard/SBChangeMonsterViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBChangeMonsterViewController.h
+//  scoreboard
+//
+//  Created by James Campagno on 8/18/15.
+//  Copyright (c) 2015 Gamesmith, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBChangeMonsterViewController : UIViewController
+
+@end
---
+++ scoreboard/SBChangeMonsterViewController.m
@@ -0,0 +1,31 @@
+//
+//  SBChangeMonsterViewController.m
+//  scoreboard
+//
+//  Created by James Campagno on 8/18/15.
+//  Copyright (c) 2015 Gamesmith, LLC. All rights reserved.
+//
+
+#import "SBChangeMonsterViewController.h"
+
+@interface SBChangeMonsterViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *changeMonster;
+
+@end
+
+@implementation SBChangeMonsterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
