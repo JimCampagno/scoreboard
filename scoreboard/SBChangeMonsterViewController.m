d7f6bc7c974de984939d18ac73a1ad44f8c19b41
JimCampagno <jimcampagno@gmail.com>
2015-10-11 22:31:28 -0400

    Game screen major changes Leave game & go back button equal spacing Room# label now is same width as other labels Change colors of Room# and choose monster labels

--- scoreboard/SBChangeMonsterViewController.m
+++ scoreboard/SBChangeMonsterViewController.m
@@ -63,7 +63,8 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
 #pragma mark - Setting up views
 
 - (void)setupBlurredViewToContainMonsters {
-    self.view.backgroundColor = [UIColor colorWithRed:0.26 green:0.43 blue:0.56 alpha:0.9];
+//    self.view.backgroundColor = [UIColor colorWithRed:0.26 green:0.43 blue:0.56 alpha:0.9];
+    self.view.backgroundColor = [UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:0.8];
     self.changeMonsterView = [[UIView alloc] init];
     self.changeMonsterView.backgroundColor = [UIColor colorWithRed:0.42 green:0.45 blue:0.47 alpha:0.97];
     [self.view addSubview:self.changeMonsterView];
@@ -129,7 +130,7 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     monsterLabel.layer.borderWidth = 0.2f;
     monsterLabel.layer.cornerRadius = 10.0f;
     monsterLabel.clipsToBounds = YES;
-    monsterLabel.textColor = [UIColor colorWithRed:0.98 green:0.8 blue:0 alpha:1];
+    monsterLabel.textColor = [[UIColor whiteColor] colorWithAlphaComponent:0.8];
     
     monsterLabel.numberOfLines = 1;
     monsterLabel.adjustsFontSizeToFitWidth = YES;
@@ -148,7 +149,7 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     UILabel *roomNumber = [UILabel new];
     roomNumber.textAlignment = NSTextAlignmentCenter;
     roomNumber.text = [NSString stringWithFormat:@"ROOM# %@", self.roomID];
-    [roomNumber setFont:[UIFont systemFontOfSize:24]];
+    [roomNumber setFont:[UIFont systemFontOfSize:20]];
     roomNumber.backgroundColor = [UIColor colorWithRed:0.42 green:0.45 blue:0.47 alpha:0.97];
     
     roomNumber.layer.borderColor = [UIColor blackColor].CGColor;
@@ -165,7 +166,7 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     [self.view addSubview:roomNumber];
     
     [roomNumber mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.equalTo(self.changeMonsterView).multipliedBy(0.75);
+        make.width.equalTo(self.changeMonsterView);
         make.bottom.equalTo(monsterLabel.mas_top).with.offset(-2);
         make.left.equalTo(self.changeMonsterView);
         make.height.equalTo(@35);
@@ -183,7 +184,7 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     cancelButton.layer.borderWidth = 0.2f;
     cancelButton.layer.cornerRadius = 10.0f;
     
-    cancelButton.titleLabel.font = [UIFont systemFontOfSize:18.0];
+    cancelButton.titleLabel.font = [UIFont systemFontOfSize:20.0];
     [cancelButton setTitleColor:[UIColor colorWithRed:0.98 green:0.8 blue:0 alpha:1] forState:UIControlStateNormal];
     
     [cancelButton setTitle:@"LEAVE GAME"
@@ -197,8 +198,9 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     
     [cancelButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.changeMonsterView.mas_bottom).with.offset(2);
-        make.left.equalTo(self.changeMonsterView).with.offset(20);
-        make.width.equalTo(self.changeMonsterView).multipliedBy(0.4);
+        make.left.equalTo(self.changeMonsterView);
+        make.right.equalTo(self.changeMonsterView.mas_centerX).with.offset(-1);
+        make.height.equalTo(@35);
     }];
     
     UIButton *goBackButton = [UIButton buttonWithType:UIButtonTypeSystem];
@@ -212,7 +214,7 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     goBackButton.layer.borderWidth = 0.2f;
     goBackButton.layer.cornerRadius = 10.0f;
     
-    goBackButton.titleLabel.font = [UIFont systemFontOfSize:18.0];
+    goBackButton.titleLabel.font = [UIFont systemFontOfSize:20.0];
     [goBackButton setTitleColor:[UIColor colorWithRed:0.98 green:0.8 blue:0 alpha:1] forState:UIControlStateNormal];
     
     [goBackButton setTitle:@"GO BACK"
@@ -226,8 +228,9 @@ static const CGFloat WidthOfMonsterButtonDivisor = 0.5;
     
     [goBackButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.changeMonsterView.mas_bottom).with.offset(2);
-        make.right.equalTo(self.changeMonsterView).with.offset(-20);
-        make.width.equalTo(self.changeMonsterView).multipliedBy(0.4);
+        make.right.equalTo(self.changeMonsterView);
+        make.left.equalTo(self.changeMonsterView.mas_centerX).with.offset(1);
+        make.height.equalTo(@35);
     }];
 }
 
